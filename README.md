57a261ebd62521993a8f9021d96b54f1f2e3f611
suikax86 <suikax86>
2024-10-19 17:39:35 +0700

    small changes

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 //TODO: 
 
-//DONE: Changed the return type of the method in the Service class to a custom response class and handled custom exception. Commit ID: []
+//DONE: Changed the return type of the method in the Service class to a custom response class and handled custom exception. Commit ID: [619c109](https://github.com/HiIamPhuc/Intro2se/commit/619c109f195336a3cf0557e0d87f24295962e27a)
--- server/src/main/java/com/suika/englishlearning/repository/UserRepository.java
+++ server/src/main/java/com/suika/englishlearning/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.suika.englishlearning.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(String email);
     Boolean existsByEmail(String email);
 }
