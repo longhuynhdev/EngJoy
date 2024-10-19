cfaaefe1d98534e1b244afc9e3378c5d92351968
suikax86 <suikax86@outlook.com>
2024-10-19 19:39:21 +0700

    small changes

--- server/src/main/java/com/suika/englishlearning/model/UserEntity.java
+++ server/src/main/java/com/suika/englishlearning/model/UserEntity.java
@@ -6,23 +6,26 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String userName;
+    @Email
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "role_id", referencedColumnName = "id")
     private Role role;
 }
