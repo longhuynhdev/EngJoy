cb4102f0365c0b58263fc098bb130b4dbb01c7b2
SalutJeSuisDaniel <ngphuc222@clc.fitus.edu.vn>
2024-11-22 22:09:49 +0700

    User Entity

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,17 +1,7 @@
-import LessonsPagination from "@/components/lessons/LessonsPagination";
-import LessonCard from "@/components/lessons/LessonCard";
-
 const HomePage = () => {
   return (
     <div>
       <h1>Home page</h1>
-      <LessonCard
-        title="em la domdom, em yeu anh jack j97"
-        body="Negav comeback nhe"
-        difficulty={["Phap Kieu", "HurryKNG", "HIEUTHUHAI"]}
-        category={["MasterD", "Hung Huynh", "Quan A.P"]}
-      />
-      <LessonsPagination></LessonsPagination>
     </div>
   );
 };
